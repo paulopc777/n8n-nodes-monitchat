611395a93d57d4ee9e2b896d5db4180ced5d348f
Paulo Cesar Granzieri <62563753+paulopc777@users.noreply.github.com>
2025-05-12 12:52:40 -0300

    Update

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 module.exports = {
-    packageName: 'n8n-nodes-monitchat',
-    nodeTypes: {
-        credentials: [
-            'dist/credentials/Monitchat.credentials.js'
-        ],
-        nodes: [
-            'dist/nodes/Monitchat/Monitchat.node.js',
-            'dist/nodes/Monitchat/SendMessage.node.js'
-        ]
-    }
+	packageName: 'n8n-nodes-monitchat',
+	nodeTypes: {
+		credentials: ['dist/credentials/Monitchat.credentials.js'],
+		nodes: ['dist/nodes/Monitchat/Monitchat.node.js'],
+	},
 };
